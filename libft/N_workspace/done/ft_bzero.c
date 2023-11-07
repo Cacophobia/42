20b764133fa6f90462e93505bccaec34b7c35a5b
Nathan Ranna <nranna@c2r3s8.42.rio>
2023-11-07 16:30:09 -0300

    bzero

--- libft/N_workspace/done/ft_bzero.c
+++ libft/N_workspace/done/ft_bzero.c
@@ -1,47 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nranna <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/25 13:43:17 by nranna            #+#    #+#             */
-/*   Updated: 2023/10/25 13:54:06 by nranna           ###   ########.fr       */
+/*   Created: 2023/11/07 15:18:14 by nranna            #+#    #+#             */
+/*   Updated: 2023/11/07 15:52:43 by nranna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+//#include "libft.h"//
 #include <string.h>
 
-void	*ft_memcpy(void *dest, const void *src, int n)
+void	ft_bzero(void *s, size_t n)
 {
-	int	counter;
-	unsigned char	*pdest;
-	unsigned char	*psrc;
+	size_t	counter;
 
 	counter = 0;
-	psrc = (unsigned char *)src;
-	pdest = (unsigned char *)dest;
 	while (counter < n)
 	{
-		*pdest = *psrc;
+		((char *)s)[counter] = '\0';
 		counter++;
 	}
-	return (dest);
 }
 
 int	main(void)
 {
-	char	src[] = "Nathan";
-	char	dest[7];
-
-	memcpy(dest, src, sizeof(src));
-	printf("Source: %s\n", src);
-	printf("Destination: %s\n", dest);
-
-	ft_memcpy(dest, src, sizeof(src));
-	printf("\nNow for my memcpy.\n");
-	printf("Source: %s\n", src);
-	printf("Destination: %s\n", dest);
+	char	test[] = "test";
+	char	test1[] = "test";
+	bzero (test, sizeof(test));
+	ft_bzero (test1, sizeof(test1));
 	return (0);
 }
