f44c3203d029a1d658fd2509f961157662fb9866
Nathan_G60s <nathan.s.ranna@gmail.com>
2023-11-09 20:29:25 -0300

    memmove

--- undone/ft_memmove.c
+++ undone/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: nranna <nranna@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 15:47:44 by nranna            #+#    #+#             */
-/*   Updated: 2023/11/08 16:23:13 by nranna           ###   ########.fr       */
+/*   Updated: 2023/11/09 07:22:50 by nranna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,24 @@ void	*memmove(void *dest, const void *src, size_t n)
 {
 	const char	*psrc;
 	char *pdest;
+
+	if (pdest = NULL && psrc = NULL)
+		return (NULL);
+	else if (pdest < psrc)
+	{
+		while ((n - 1) > 0)
+		{
+			*(pdest++) = *(psrc++);
+		}
+	}
+	else if (pdest > psrc)
+	{
+		pdest = pdest + n;
+		psrc = psrc + n;
+		while ((n - 1) > 0)
+		{
+			*(pdest--) = *(psrc--);
+		}
+	}
+	return (dest)
 }
