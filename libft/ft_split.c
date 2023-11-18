73a57572ef844b8d61c81066f5c2258bb85118b2
Nathan Ranna <nranna@c2r3s8.42.rio>
2023-11-18 05:59:15 -0300

    wtf

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: nranna <nranna@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 21:14:10 by nranna            #+#    #+#             */
-/*   Updated: 2023/11/18 05:01:51 by nranna           ###   ########.fr       */
+/*   Updated: 2023/11/18 05:52:23 by nranna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ static char	*word_maker(char const *s, int start, int finish)
 	size_t	counter;
 
 	counter = 0;
-	substr = ft_calloc((finish - start + 1) * sizeof(char));
+	substr = malloc((finish - start + 1) * sizeof(char));
 	if (!substr)
 		return (NULL);
 	while (start < finish)
@@ -59,7 +59,7 @@ char	**ft_split(char const *s, char c)
 
 	if (!s)
 		return (NULL);
-	substrings = malloc((word_counter(s, c) + 1) * sizeof(char *));
+	substrings = ft_calloc((word_counter(s, c) + 1), sizeof(char *));
 	if (!substrings)
 		return (NULL);
 	x = 0;
