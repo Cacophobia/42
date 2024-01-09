cb6b323a291293b4dff8a5acbf6e73d120e8c91c
Nathan Ranna <nranna@c1r4s2.42.rio>
2024-01-09 18:17:42 -0300

    redo gnl

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -6,34 +6,10 @@
 /*   By: nranna <nranna@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/31 17:23:10 by nranna            #+#    #+#             */
-/*   Updated: 2024/01/06 10:43:26 by nranna           ###   ########.fr       */
+/*   Updated: 2024/01/09 15:25:32 by nranna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-
-//You'll have to study linked lists :)
-//char	*get_next_line(int fd)
-int	main(void)
+char	*get_next_line(int fd)
 {
-	int	fd;
-	char	buf[1024];
-	int	chars_read;
-
-	chars_read = 0;
-	fd = open("test.txt", O_RDONLY);
-	//Changing this last value in while function makes
-	//Your basic GNL fetch more characters.
-	//chars_read = read(fd, buf, 2);
-	//buf[chars_read] = '\n';
-	//buf[chars_read + 1] = '\0';
-	//printf("%s", buf);
-	while (chars_read = read(fd, buf, 20))
-	{
-		buf[chars_read] = '\0';
-		printf("buf -> %s", buf);
-	}
-	return (0);
 }
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -6,13 +6,12 @@
 /*   By: nranna <nranna@student.42.rio>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/31 17:24:00 by nranna            #+#    #+#             */
-/*   Updated: 2023/12/31 19:54:57 by nranna           ###   ########.fr       */
+/*   Updated: 2024/01/09 15:27:38 by nranna           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE
 
-char	*get_next_line(int fd);
 
 #endif
